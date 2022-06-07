6faaf370e302624379ee6b216b2edb4d8a170455
IkhtiyorDev <ikhtiyordev@gmail.com>
2022-06-07 20:35:26 +0500

    first commit

---
+++ order-service/src/test/java/dev/ikhtiyor/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
